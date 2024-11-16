08d050f6cf196901eb9f7d34ea399f7de5d4a22f
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-11-16 20:47:10 +0300

    using state for dynamic rendering

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { useState } from "react";
 import DataGraph from "../components/DataGraph";
 
 const Data = () => {
+  const [compareWith, setCompareWith] = useState("Compare With Yourself");
+  
+
+  const handleCompareWith = (e) => {
+    setCompareWith(e.target.value);
+  };
+
   return (
     <div className="p-8 space-y-6 bg-gray-100 min-h-screen">
       <div className="grid grid-cols-1 md:grid-cols-5 gap-4 text-gray-700 text-sm">
@@ -23,7 +30,7 @@ const Data = () => {
       </div>
 
       <div className="flex items-center justify-between">
-        <div className="grid grid-cols-1 md:grid-cols-3  text-gray-700 text-sm">
+        <div className="grid grid-cols-1 md:grid-cols-3 text-gray-700 text-sm">
           <div className="pr-44">
             <span className="font-semibold">Swimmer:</span> Chloe
           </div>
@@ -35,52 +42,80 @@ const Data = () => {
           </div>
         </div>
         <div>
-          <select className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400">
+          <select
+            className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            value={compareWith}
+            onChange={handleCompareWith}
+          >
             <option value="Compare With Yourself">Compare With Yourself</option>
             <option value="Compare With Others">Compare With Others</option>
-            <option value="Compare With Pose Analysis">Pose Analysis</option>
+            <option value="Pose Analysis">Pose Analysis</option>
           </select>
         </div>
       </div>
 
-      <div className="text-center">
-      </div>
+      <div className="text-center"></div>
 
-      <div className="flex flex-wrap justify-between items-center space-y-4 md:space-y-0">
+      <div className="flex justify-between items-center">
         <h2 className="text-xl font-bold">4X50 Freestyle @ 1:00</h2>
-        <div className="flex flex-wrap">
-        <div className="flex items-center space-x-4 pr-5">
-          <label htmlFor="target" className="font-semibold text-gray-700">
-            Target:
-          </label>
-          <select
-            id="target"
-            className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-          >
-            <option value="previousAverage">Previous Average</option>
-            <option value="personalBest">Personal Best</option>
-            <option value="goal">Goal</option>
-          </select>
+        {compareWith === "Pose Analysis" && (
+          <div className="flex space-x-6 text-sm text-gray-700">
+            <div>
+              <span className="font-semibold">Style:</span> Freestyle
+            </div>
+            <div>
+              <span className="font-semibold">Type:</span> All
+            </div>
+            <div>
+              <span className="font-semibold">View:</span> Side
+            </div>
+          </div>
+        )}
+   
+
+      {compareWith !== "Pose Analysis" && (
+        <div className="flex flex-wrap md:flex-nowrap justify-between items-center space-y-4 md:space-y-0">
+          <div className="flex items-center space-x-4 pr-5">
+            <label htmlFor="target" className="font-semibold text-gray-700">
+              Target:
+            </label>
+            {compareWith === "Compare With Yourself" ? (
+              <select
+                id="target"
+                className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              >
+                <option value="previousAverage">Previous Average</option>
+                <option value="personalBest">Personal Best</option>
+                <option value="goal">Goal</option>
+              </select>
+            ) : (
+              <select
+                id="target"
+                className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              >
+                <option value="previousAverage">Classmates</option>
+              </select>
+            )}
+          </div>
+          <div className="flex items-center space-x-4">
+            <label htmlFor="attribute" className="font-semibold text-gray-700">
+              Attribute:
+            </label>
+            <select
+              id="attribute"
+              className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            >
+              <option value="lapTime">Lap Time</option>
+              <option value="strokeRate">Stroke Rate</option>
+              <option value="distance">Distance</option>
+            </select>
+          </div>
         </div>
-        <div className="flex items-center space-x-4">
-          <label htmlFor="attribute" className="font-semibold text-gray-700">
-            Attribute:
-          </label>
-          <select
-            id="attribute"
-            className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-          >
-            <option value="lapTime">Lap Time</option>
-            <option value="strokeRate">Stroke Rate</option>
-            <option value="distance">Distance</option>
-          </select>
-        </div>
-        </div>
-      </div>
+      )}
+         </div>
 
       <div className="bg-white shadow-md rounded-lg p-6">
-        <div className=" flex flex-col items-center justify-center border border-gray-300 rounded-md">
-          
+        <div className="flex flex-col items-center justify-center border border-gray-300 rounded-md">
           <DataGraph />
         </div>
       </div>
