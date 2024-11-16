7f88478bfad5f9e05b3c8fd15230cdeee1e14d8a
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-11-16 12:09:28 +0300

    Home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Data from "./pages/Data";
 import Coaching from "./pages/Coaching";
+import Home from "./pages/Home";
 function App() {
   return (
     <Routes>
       <Route path="/data" element={<Data />} />
       <Route path="/coaching" element={<Coaching />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
