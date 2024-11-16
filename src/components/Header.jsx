22e375b9dd8d720fa4a5eec58a7da4e17027da83
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-11-16 16:52:20 +0300

    Header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      <div className="bg-gray-100 h-16 flex justify-between items-center p-6">
+      <div className="bg-gray-100 h-16 flex justify-between items-center p-6 pl-16">
         <nav className="flex flex-grow justify-start">
           <ul className="flex space-x-8">
             <li>
@@ -14,7 +14,7 @@ const Header = () => {
                 AI4Swim
               </Link>
             </li>
-            <li>
+            <li className="opacity-60">
               <Link
                 to="/data"
                 className="text-gray-700 hover:text-blue-500 text-lg"
@@ -22,15 +22,16 @@ const Header = () => {
                 Live Dashboard
               </Link>
             </li>
-            <li>
+            <li className="relative">
               <Link
                 to="/data"
-                className="text-gray-700 hover:text-blue-500 text-lg"
+                className="text-gray-700 hover:text-blue-500 text-lg font-semibold"
               >
                 Live Coaching
               </Link>
+              <div className="absolute bottom-0 left-0 w-full h-0.5 bg-blue-500 -mb-2"></div>
             </li>
-            <li>
+            <li className="opacity-60">
               <Link
                 to="/data"
                 className="text-gray-700 hover:text-blue-500 text-lg"
@@ -42,13 +43,11 @@ const Header = () => {
         </nav>
 
         <div className="flex items-center space-x-4">
-        <img 
-  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKaiKiPcLJj7ufrj6M2KaPwyCT4lDSFA5oog&s" 
-  alt="Avatar" 
-  className="w-10 h-10 rounded-full" 
-/>
-
-
+          <img 
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKaiKiPcLJj7ufrj6M2KaPwyCT4lDSFA5oog&s" 
+            alt="Avatar" 
+            className="w-10 h-10 rounded-full" 
+          />
           <span className="text-gray-700 text-lg font-semibold">John Doe</span>
         </div>
       </div>
