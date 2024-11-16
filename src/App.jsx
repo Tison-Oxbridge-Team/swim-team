da777149ce2ef61c28141c0547488821a91fece9
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-11-16 11:19:42 +0300

    initialized with vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="font-bold text-6xl text-green-500">Swim Team</h1>
+    </div>
+  );
+}
+
+export default App;
